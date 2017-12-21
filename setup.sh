7ce3fcfd66f0b81f89303509faeb08f8c63ec660
Aditya Pratama <muhammadfanyadityapratama@gmail.com>
2017-12-21 08:41:26 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ _x86 () {
 sleep 0.2
 echo "$INT Installing SuperSU (x86)..."
 cd $SUBIN
-if [ -f su ]; then rm -f su 2> /dev/null; fi
+if [ -f su ]; then rm -f su; fi
 cp $SU/x86/su.pie su
 chmod 4751 su
 su --install
@@ -30,7 +30,7 @@ _x64 () {
 sleep 0.2
 echo "$INT Installing SuperSU (x64)..."
 cd $SUBIN
-if [ -f su ]; then rm -f su 2> /dev/null; fi
+if [ -f su ]; then rm -f su; fi
 cp $SU/x64/su .
 chmod 4751 su
 su --install
@@ -39,23 +39,29 @@ su --daemon
 sleep 1.2
 echo
 echo "$INT Preparing..."
-if [ -d $SU ]; then rm -rf $SU; else mkdir -p $SU; fi
-	
+if [ -d $SU ]; then rm -rf $SU; else mkdir -p $SU 2> /dev/null; fi
+sleep 0.1
+if [ ! -d $SU ]; then echo "$INT Can't create folder, permission denied..."; exit; fi
 echo "$INT Downloading SuperSU, please wait..."
 sleep 0.5
 wget -qP $SU http://phoenix.de4ce.gq/su.zip 2> /dev/null
-if [ ! -f $SU/su.zip ]; then echo "$INT File error..."; exit; fi
+if [ ! -f $SU/su.zip ]; then echo "$INT File not found..."; exit; fi
 MD5SUM="8755c94775431f20bd8de368a2c7a179  $SU/su.zip"
 FILE_MD5SUM="$(md5sum $SU/su.zip)"
 
 if [ "$MD5SUM" != "$FILE_MD5SUM" ]; then echo "$INT File Error..."; exit; fi
 echo "$INT Extracting zip..."
 sleep 0.2
-unzip -q su.zip -d $SU 2> /dev/null
+unzip -q $SU/su.zip -d $SU 2> /dev/null
+
+if [ ! -d $SU/common ]; then echo "$INT Extract failed..."; exit; fi
+if [ ! -d $SU/x64 ]; then echo "$INT Extract failed..."; exit; fi
+if [ ! -d $SU/x86 ]; then echo "$INT Extract failed..."; exit; fi
+
+
 ARCH=$(uname -m)
 if [ $ARCH = x86_64 ]; then _x64; else _x86; fi
 pm install $SU/common/Superuser.apk 2> /dev/null
-rm -rf $SU
 sleep 1
 echo "$INT SuperSU successfully installed."
 exit
